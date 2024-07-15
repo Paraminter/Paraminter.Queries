e8389171ec8f03e373b169c230e619fae515dc3d
ErikWe <erikwestberg7@gmail.com>
2024-07-15 17:59:21 +0200

    Name "QueryResponseCollector" rather than "ResponseCollector"

--- src/Handlers/IQueryHandler.cs
+++ src/Handlers/IQueryHandler.cs
@@ -4,13 +4,13 @@ using Paraminter.Queries.Collectors;
 
 /// <summary>Handles queries.</summary>
 /// <typeparam name="TQuery">The type of the handled queries.</typeparam>
-/// <typeparam name="TResponseCollector">The type collecting the responses from handling queries.</typeparam>
-public interface IQueryHandler<in TQuery, in TResponseCollector>
+/// <typeparam name="TQueryResponseCollector">The type collecting the responses from handling queries.</typeparam>
+public interface IQueryHandler<in TQuery, in TQueryResponseCollector>
     where TQuery : IQuery
-    where TResponseCollector : IResponseCollector
+    where TQueryResponseCollector : IQueryResponseCollector
 {
     /// <summary>Handles the provided query.</summary>
     /// <param name="query">The handled query.</param>
-    /// <param name="responseCollector">Collects the response from handling the query.</param>
-    public abstract void Handle(TQuery query, TResponseCollector responseCollector);
+    /// <param name="queryResponseCollector">Collects the response from handling the query.</param>
+    public abstract void Handle(TQuery query, TQueryResponseCollector queryResponseCollector);
 }
