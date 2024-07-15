cd2f90014102b8d46fe633f7b90729f6d79a8de7
ErikWe <erikwestberg7@gmail.com>
2024-07-15 15:50:30 +0200

    Handlers use response-collector rather than return value

--- src/Handlers/Abstractions/IQueryHandler.cs
+++ src/Handlers/Abstractions/IQueryHandler.cs
@@ -2,12 +2,12 @@
 
 /// <summary>Handles queries.</summary>
 /// <typeparam name="TQuery">The type of the handled queries.</typeparam>
-/// <typeparam name="TResponse">The type of the response of handled queries.</typeparam>
-public interface IQueryHandler<in TQuery, out TResponse>
+/// <typeparam name="TResponse">The type collecting the responses from handling queries.</typeparam>
+public interface IQueryHandler<in TQuery, in TResponseCollector>
     where TQuery : IQuery
 {
     /// <summary>Handles the provided query.</summary>
     /// <param name="query">The handled query.</param>
-    /// <returns>The response of the handled query.</returns>
-    public abstract TResponse Handle(TQuery query);
+    /// <param name="responseCollector">Collects the response from handling the query.</param>
+    public abstract void Handle(TQuery query, TResponseCollector responseCollector);
 }
